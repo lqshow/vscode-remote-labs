908db082a02cbd78f942d3aae9541fb9e108466d
qiong.lin <qiong.lin@basebit.ai>
2021-12-19 03:09:05 +0000

    chore: Add run-devbox.sh

---
+++ .devcontainer/run-devbox.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  bin="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$bin/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+bin="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $bin
+
+# Setting ssh config
+mkdir -p /root/.ssh && cp -r /root/.ssh-localhost/* /root/.ssh && chmod 700 /root/.ssh && chmod 600 /root/.ssh/*
